92d056715a06ebd414e764c664691397a14c731b
Jacobo Vasquez <jacobo.vasquez@asap507.com>
2023-06-18 07:20:20 -0400

    FIX in credentials not found

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -5,7 +5,7 @@ import path from 'path';
 class FirebaseDB implements IDB<typeof admin.database> {
     private instance: any;
     constructor() {
-        const serviceAccount = path.join(__dirname, 'credentials/atom-lost-firebase-adminsdk-dg3tm-8ce5ff0a4d.json');
+        const serviceAccount = path.join(__dirname, '/../../atom-lost-firebase-adminsdk-dg3tm-8ce5ff0a4d.json');
         admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
             databaseURL: "https://atom-lost-default-rtdb.firebaseio.com"
--- src/db/firestore.ts
+++ src/db/firestore.ts
@@ -5,7 +5,7 @@ import path from 'path';
 class FirestoreDB implements IDB<any> {
   private instance: any;
   constructor() {
-    const serviceAccount = path.join(__dirname, 'credentials/atom-test-2cf92-firebase-adminsdk-qkff5-f595bd8f87.json');
+    const serviceAccount = path.join(__dirname, '/../../credentials/atom-test-credential.json');
     initializeApp({
       credential: cert(serviceAccount),
     });
